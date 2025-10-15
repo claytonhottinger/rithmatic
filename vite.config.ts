510b139812b0b136154acba98341b51752b16b4b
Clayton Hottinger <funkybarracuda@gmail.com>
2025-10-15 16:29:06 -0600

    Add base to CI builds

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.CI ? '/rithmatic/' : '/',
   plugins: [
     vue(),
     vueJsx(),
